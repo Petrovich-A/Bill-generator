34f0c466b4fce09585d796a2c2b483328415bc81
Alexandr <a.piatrovich@gmail.com>
2023-01-09 22:15:51 +0300

    updated SQL scripts

--- src/main/resources/database/MySQL data base script.sql
+++ src/main/resources/database/MySQL data base script.sql
@@ -1,52 +1,22 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-
--- -----------------------------------------------------
--- Schema billgenerator
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema billgenerator
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `billgenerator` DEFAULT CHARACTER SET utf8 ;
 USE `billgenerator` ;
 
--- -----------------------------------------------------
--- Table `billgenerator`.`discount_cards`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `billgenerator`.`discount_cards` (
-  `id_discount_card` INT NOT NULL AUTO_INCREMENT,
-  `number` INT NOT NULL,
-  `discount_percent` DOUBLE NOT NULL,
-  PRIMARY KEY (`id_discount_card`),
-  UNIQUE INDEX `id_discount_card_UNIQUE` (`id_discount_card` ASC),
-  UNIQUE INDEX `number_UNIQUE` (`number` ASC))
-ENGINE = InnoDB
-AUTO_INCREMENT = 4
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `billgenerator`.`products`
--- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `billgenerator`.`products` (
-  `id_product` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
+  `id_product` INT NOT NULL AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+  `name` VARCHAR(45) NOT NULL UNIQUE,
   `prise` DOUBLE NOT NULL,
-  `is_on_sale` TINYINT NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id_product`),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC),
-  UNIQUE INDEX `id_product_UNIQUE` (`id_product` ASC))
-ENGINE = InnoDB
-AUTO_INCREMENT = 9
-DEFAULT CHARACTER SET = utf8mb3;
+  `is_on_sale` TINYINT NOT NULL DEFAULT '0')
+AUTO_INCREMENT = 0;
 
--- -----------------------------------------------------
--- Populate Table `products`
--- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `billgenerator`.`discount_cards` (
+  `id_discount_card` INT  NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
+  `number` INT NOT NULL UNIQUE,
+  `discount_percent` DOUBLE NOT NULL)
+  AUTO_INCREMENT = 0;
+  
+insert into discount_cards (number, discount_percent) values (1234, 10);
+insert into discount_cards (number, discount_percent) values (1235, 8);
+insert into discount_cards (number, discount_percent) values (1236, 2.5);
 
 insert into products (name, prise ) values ('Kindness', 7.25);
 insert into products (name, prise ) values ('Solidarity', 4.15);
@@ -55,15 +25,3 @@ insert into products (name, prise ) values ('Empathy', 4.00);
 insert into products (name, prise ) values ('Humility', 9.50);
 insert into products (name, prise ) values ('Intelligence', 1.1);
 insert into products (name, prise, is_on_sale ) values ('Benevolence', 2.50, 1);
-
--- -----------------------------------------------------
--- Populate Table `discount_cards`
--- -----------------------------------------------------
-
-insert into discount_cards (number, discount_percent) values (1234, 10);
-insert into discount_cards (number, discount_percent) values (1235, 8);
-insert into discount_cards (number, discount_percent) values (1236, 2.5);
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
--- src/main/resources/database/PostgreSQL data base script.sql
+++ src/main/resources/database/PostgreSQL data base script.sql
@@ -1,20 +1,23 @@
+CREATE SCHEMA IF NOT EXISTS billgenerator;
+USE billgenerator;
+
 CREATE TABLE IF NOT EXISTS discount_cards
 (
   id_discount_card SERIAL PRIMARY KEY UNIQUE,
   number INTEGER UNIQUE NOT NULL,
   discount_percent NUMERIC(5,2) NOT NULL CHECK(discount_percent > 0)
 );
-  
-INSERT INTO discount_cards (number, discount_percent) values (1234, 10);
-INSERT INTO discount_cards (number, discount_percent) values (1235, 8);
-INSERT INTO discount_cards (number, discount_percent) values (1236, 2.5);
 
-CREATE TABLE IF NOT EXISTS products (
+ CREATE TABLE IF NOT EXISTS products (
   id_product SERIAL PRIMARY KEY UNIQUE,
   name VARCHAR(45) NOT NULL,
   prise NUMERIC(5,2) NOT NULL CHECK(prise > 0),
   is_on_sale BOOLEAN NOT NULL DEFAULT FALSE
-) ;
+) ; 
+
+INSERT INTO discount_cards (number, discount_percent) values (1234, 10);
+INSERT INTO discount_cards (number, discount_percent) values (1235, 8);
+INSERT INTO discount_cards (number, discount_percent) values (1236, 2.5);
 
 insert into products (name, prise, is_on_sale ) values ('Aggressiveness', 0.27, TRUE);
 insert into products (name, prise) values ('Arrogance', 0.87);
